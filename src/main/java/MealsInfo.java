7c6367233201b3165e8a7b257812d70b2727a972
Anelairam <m.livathinopoulou@gmail.com>
2023-03-02 11:04:37 +0100

    Save & edit functionality removed from SearchByName, showDetails functionallity updated. Save & edit functionality added to MealsInfo

--- src/main/java/MealsInfo.java
+++ src/main/java/MealsInfo.java
@@ -17,9 +17,10 @@ public class MealsInfo extends JFrame{
     private JTextArea strInstructions;
     private JButton btnBack;
     private JButton saveData;
+    private JButton editBtn;
+    private JButton deleteBtn;
 
     private String userSearchValue;
-
 
     public MealsInfo() {
         setContentPane(mealsInfo);
@@ -46,14 +47,77 @@ public class MealsInfo extends JFrame{
 
                 if (JOptionPane.showConfirmDialog( frame,"Save changes to Database?","MealsAPP",
                         JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION) {
+//                    Firstly checks if the data exist in the DB
                     int idm = Integer.valueOf(idMeal.getText());
-                    String strm = strMeal.getText();
-                    String stra = strMealArea.getText();
-                    String strc = strCate.getText();
-                    String stri = strInstructions.getText();
-                    DBfunctions.UpdateMeal(idm, strm, stra, strc, stri);
-                    System.out.println("update db");
+                    boolean found = false;
+                    DBfunctions.selectMeal(idm, found);
+                    if (DBfunctions.getfound() == true) {
+                        String strm = strMeal.getText();
+                        String stra = strMealArea.getText();
+                        String strc = strCate.getText();
+                        String stri = strInstructions.getText();
+                        DBfunctions.UpdateMeal(idm, strm, stra, strc, stri);
+//                    Disable editable fields
+                        strMeal.setEditable(false);
+                        strCate.setEditable(false);
+                        strMealArea.setEditable(false);
+                        strInstructions.setEditable(false);
+//                    Updating visually the fields that changed
+                        strMeal.setText(strMeal.getText());
+                        strCate.setText(strCate.getText());
+                        strMealArea.setText(strMealArea.getText());
+                        strInstructions.setText(strInstructions.getText());
+                        System.out.println("update db");
+                    }else {
+                        String strm = strMeal.getText();
+                        String stra = strMealArea.getText();
+                        String strc = strCate.getText();
+                        String stri = strInstructions.getText();
+                        DBfunctions.insertNewMeal(idm, strm, stra, strc, stri );
+                        System.out.println("save db");
+                    }
+
+
                 }
+            }
+        });
+        editBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (e.getSource() == editBtn) {
+//                    dispose();
+                    JFrame frame = new JFrame("Edit");
+                    if (JOptionPane.showConfirmDialog( frame,"Edit Meal ?","MealsAPP",
+                            JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION) {
+                        int idm = Integer.valueOf(idMeal.getText());
+                        boolean found = false;
+                        System.out.println("Before Id : "+ idm + " Found : " + found);
+                        DBfunctions.selectMeal(idm, found);
+                        int idM = DBfunctions.getIdMeal();
+                        String strM = DBfunctions.getStrMeal();
+                        String strA = DBfunctions.getStrCategory();
+                        String strC = DBfunctions.getStrArea();
+                        String strI = DBfunctions.getStrInstructions();
+                        boolean sedit = true;
+                        System.out.println();
+
+                        if (DBfunctions.getfound() == true) {
+                            strMeal.setEditable(true);
+                            strCate.setEditable(true);
+                            strMealArea.setEditable(true);
+                            strInstructions.setEditable(true);
+//                            MealsInfo meal = new MealsInfo();
+//                            meal.setMealInfo(idM, strM, strA, strC, strI, sedit);
+//                            meal.setVisible(true);
+                        }
+                    }
+                }
+            }
+        });
+        deleteBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
             }
         });
     }
--- src/main/java/searchByName.java
+++ src/main/java/searchByName.java
@@ -10,9 +10,9 @@ public class searchByName extends JFrame{
     private JTextField nameSearchValue;
     private JButton searchButton;
     private JList displayView;
-    private JButton saveData;
+//    private JButton saveData;
     private JButton deleteData;
-    private JButton editMealButton;
+//    private JButton editMealButton;
     private JButton viewDBButton;
     private JScrollPane scroll;
     private JButton showDetails;
@@ -45,22 +45,24 @@ public class searchByName extends JFrame{
         });
 
         //save data to DB
-        saveData.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                if (displayView.getSelectedIndex() >= 0 ){
-                    int idm = MealItems.mealList.get(displayView.getSelectedIndex()).getIdmeal();
-                    String strm = MealItems.mealList.get(displayView.getSelectedIndex()).getStrmeal();
-                    String stra = MealItems.mealList.get(displayView.getSelectedIndex()).getStrarea();
-                    String strc = MealItems.mealList.get(displayView.getSelectedIndex()).getStrcat();
-                    String stri = MealItems.mealList.get(displayView.getSelectedIndex()).getStrinstr();
-                    DBfunctions.insertNewMeal(idm, strm, stra, strc, stri );
-                    System.out.println("save db");
-                }
-            }
-        });
+// <------------------------To be removed if the UAT passes---------------------------->
+//        saveData.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                if (displayView.getSelectedIndex() >= 0 ){
+//                    int idm = MealItems.mealList.get(displayView.getSelectedIndex()).getIdmeal();
+//                    String strm = MealItems.mealList.get(displayView.getSelectedIndex()).getStrmeal();
+//                    String stra = MealItems.mealList.get(displayView.getSelectedIndex()).getStrarea();
+//                    String strc = MealItems.mealList.get(displayView.getSelectedIndex()).getStrcat();
+//                    String stri = MealItems.mealList.get(displayView.getSelectedIndex()).getStrinstr();
+//                    DBfunctions.insertNewMeal(idm, strm, stra, strc, stri );
+//                    System.out.println("save db");
+//                }
+//            }
+//        });
+//<----------------------------------------------------------------------------------->
 
-        //
+
         displayView.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -98,54 +100,74 @@ public class searchByName extends JFrame{
                 DBfunctions.selectAll();
             }
         });
-
-        editMealButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                    if (e.getSource() == editMealButton) {
- //                       dispose();
-                        JFrame frame = new JFrame("Edit");
-                        if (JOptionPane.showConfirmDialog( frame,"Edit Meal ?","MealsAPP",
-                                JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION) {
-                            int idm = MealItems.mealList.get(displayView.getSelectedIndex()).getIdmeal();
-                            boolean found = false;
-                            DBfunctions.selectMeal(idm, found);
-
-                            int idM = DBfunctions.getIdMeal();
-                            String strM = DBfunctions.getStrMeal();
-                            String strA = DBfunctions.getStrCategory();
-                            String strC = DBfunctions.getStrArea();
-                            String strI = DBfunctions.getStrInstructions();
-                            boolean sedit = true;
-                            System.out.println();
-
-                            if (DBfunctions.getfound() == true) {
-                                MealsInfo meal = new MealsInfo();
-                                meal.setMealInfo(idM, strM, strA, strC, strI, sedit);
-                                meal.setVisible(true);
-                            }
-                        }
-                    }
-
-            }
-        });
-
+// <------------------------To be removed if the UAT passes---------------------------->
+//        editMealButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                    if (e.getSource() == editMealButton) {
+//                        dispose();
+//                        JFrame frame = new JFrame("Edit");
+//                        if (JOptionPane.showConfirmDialog( frame,"Edit Meal ?","MealsAPP",
+//                                JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION) {
+//                            int idm = MealItems.mealList.get(displayView.getSelectedIndex()).getIdmeal();
+//                            boolean found = false;
+//                            DBfunctions.selectMeal(idm, found);
+//
+//                            int idM = DBfunctions.getIdMeal();
+//                            String strM = DBfunctions.getStrMeal();
+//                            String strA = DBfunctions.getStrCategory();
+//                            String strC = DBfunctions.getStrArea();
+//                            String strI = DBfunctions.getStrInstructions();
+//                            boolean sedit = true;
+//                            System.out.println();
+//
+//                            if (DBfunctions.getfound() == true) {
+//                                MealsInfo meal = new MealsInfo();
+//                                meal.setMealInfo(idM, strM, strA, strC, strI, sedit);
+//                                meal.setVisible(true);
+//                            }
+//                        }
+//                    }
+//
+//            }
+//        });
+//<----------------------------------------------------------------------------------->
         //show detail button
         showDetails.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 dispose();
                 int idM = MealItems.mealList.get(displayView.getSelectedIndex()).getIdmeal();
-                String strM = MealItems.mealList.get(displayView.getSelectedIndex()).getStrmeal();
-                String strA = MealItems.mealList.get(displayView.getSelectedIndex()).getStrarea();
-                String strC = MealItems.mealList.get(displayView.getSelectedIndex()).getStrcat();
-                String strI = MealItems.mealList.get(displayView.getSelectedIndex()).getStrinstr();
-                boolean sedit = false;
-                System.out.println();
-                MealsInfo meal = new MealsInfo();
-                meal.setMealInfo(idM, strM, strA, strC, strI, sedit);
-                meal.setSearchValue(searchValue);
-                meal.setVisible(true);
+//                Control if the meal exist to return the correct information from the
+//                DB if it does from the API if it does not
+                boolean found = false;
+                DBfunctions.selectMeal(idM, found);
+                if (DBfunctions.getfound() == true){
+                    System.out.println("found in DB");
+                    int idMeal = DBfunctions.getIdMeal();
+                    String strM = DBfunctions.getStrMeal();
+                    String strA = DBfunctions.getStrCategory();
+                    String strC = DBfunctions.getStrArea();
+                    String strI = DBfunctions.getStrInstructions();
+                    boolean sedit = false;
+                    System.out.println("In DB");
+                    MealsInfo meal = new MealsInfo();
+                    meal.setMealInfo(idMeal, strM, strA, strC, strI, sedit);
+                    meal.setSearchValue(searchValue);
+                    meal.setVisible(true);
+                }
+                else{
+                    String strM = MealItems.mealList.get(displayView.getSelectedIndex()).getStrmeal();
+                    String strA = MealItems.mealList.get(displayView.getSelectedIndex()).getStrarea();
+                    String strC = MealItems.mealList.get(displayView.getSelectedIndex()).getStrcat();
+                    String strI = MealItems.mealList.get(displayView.getSelectedIndex()).getStrinstr();
+                    boolean sedit = false;
+                    System.out.println("Not in DB");
+                    MealsInfo meal = new MealsInfo();
+                    meal.setMealInfo(idM, strM, strA, strC, strI, sedit);
+                    meal.setSearchValue(searchValue);
+                    meal.setVisible(true);
+                }
             }
         });
 
